b103ec3c84bd17ce9aeab30651be97a20d2cd192
nicolasromer <nicolasromer@gmail.com>
2014-01-15 15:04:49 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Sample::Application.routes.draw do
   
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
   get '/signup', to: 'users#new'
+  get '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   get '/help',   to: 'static_pages#help'
   get '/about',  to: 'static_pages#about'
   get '/contact',to: 'static_pages#contact'
--- spec/helpers/application_helper.rb
+++ spec/helpers/application_helper.rb
@@ -8,3 +8,4 @@
 	    "#{base_title} | #{page_title}"
 	  end
 	end
+
